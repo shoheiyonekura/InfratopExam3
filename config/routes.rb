40e091429f8ae2340df80a4c311d915708895625
shoheiyonekura <38638474+shoheiyonekura@users.noreply.github.com>
2023-05-10 05:56:16 +0000

    users controller 作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :books, only: [:new,:index,:show,:edit] 
+  resources :users, only: [:show,:edit,:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
