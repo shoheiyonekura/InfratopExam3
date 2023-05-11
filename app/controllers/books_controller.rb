5a9492ba8696a37b5991cd7883b35b6c8c914fe5
shoheiyonekura <38638474+shoheiyonekura@users.noreply.github.com>
2023-05-11 07:39:45 +0000

    book-index,show編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,20 @@
 class BooksController < ApplicationController
-  
-  def new
+ 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
     
   end
   
   def index
-    
+    @book = Book.new
+    @books = Book.all
   end
   
   def show
-    
+    @book = Book.find(params[:id])
   end
   
   def edit
@@ -18,5 +23,11 @@ class BooksController < ApplicationController
   
   def destroy
     
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
