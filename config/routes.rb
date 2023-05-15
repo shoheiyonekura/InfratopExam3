c6ca2bbf4792785a774b996fc7dad8521de3e7f0
shoheiyonekura <38638474+shoheiyonekura@users.noreply.github.com>
2023-05-15 05:14:40 +0000

    header footer追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
+  get 'homes/about' => 'homes#about', as: "about"
   
   resources :books, only: [:new,:index,:show,:edit] 
   resources :users, only: [:show,:edit,:index]
