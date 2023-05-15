b34ff3a219e22cd753657766140b367e6b9c7a62
shoheiyonekura <38638474+shoheiyonekura@users.noreply.github.com>
2023-05-15 04:50:55 +0000

    edit追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,29 @@
 class BooksController < ApplicationController
- 
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to books_path
-    
   end
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+
   def show
-    @book = Book.find(params[:id])
+    @book1 = Book.find(params[:id])
   end
-  
+
   def edit
-    
+
   end
-  
+
   def destroy
-    
+
   end
-  
+
   private
 
   def book_params
